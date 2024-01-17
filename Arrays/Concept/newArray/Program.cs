8b3dcf6347572a33918f6dd260ab87589870db4b
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-17 11:14:04 -0600

    Added item count

--- Arrays/Concept/newArray/Program.cs
+++ Arrays/Concept/newArray/Program.cs
@@ -27,9 +27,12 @@ Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to p
 
 int[] inventory = { 200, 450, 700, 175, 250 };
 int sum = 0;
+int bin = 0;
 foreach (int items in inventory)
 {
     sum += items;
-    Console.WriteLine($"We have {sum} items in inventory.");
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
+    Console.WriteLine($"We have {sum} items in inventory.");
 // Console.ReadLine();

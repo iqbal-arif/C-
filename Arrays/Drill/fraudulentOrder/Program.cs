eca996ad6f7fb9e8610d87426b8f5d6fdbfdb3cb
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-17 13:47:07 -0600

    Frauduluent Order Id Initial Commit

---
+++ Arrays/Drill/fraudulentOrder/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fraudulent Order\n");
+
+string[] fraudulentOrderIDs = { "B123", "C234", "A345","C15", "B177", "G3003", "C235", "B179" };
+
+foreach(string items in fraudulentOrderIDs){
+    if (items.StartsWith("B")){
+
+    Console.WriteLine(items);
+    }
+}

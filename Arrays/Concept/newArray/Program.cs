782d7e029cc3b859dea37a42f0f17bb3bf9e7ecb
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-17 10:30:31 -0600

    Initial Commit

---
+++ Arrays/Concept/newArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Array Basics\n");
+
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+Console.ReadLine();
+

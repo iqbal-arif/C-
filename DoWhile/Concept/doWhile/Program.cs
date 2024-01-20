5b73b9d56aebfba84c8f5edb8b4003f9d9635a92
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-20 13:50:29 -0600

    Initial Commit

---
+++ DoWhile/Concept/doWhile/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Do While Loop!\n");
+/*
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine($"Inside Block {current}");
+} while (current != 7);
+    Console.WriteLine($"DoWhile Block {current}");
+
+*/
+Console.WriteLine("\nWhile Loop!\n");
+Random randomDo = new Random();
+// int currentDo = randomDo.Next(1, 11);
+int currentDo = 0;
+
+while (currentDo >= 3)
+{
+    Console.WriteLine($"Inside Block {currentDo}");
+    currentDo = randomDo.Next(1, 20);
+} 
+    Console.WriteLine($"While Block {currentDo}\n");
+
+
+Console.WriteLine("\nWhile Loop with Continue Statement!\n");
+
+Random randomCount = new Random();
+int currentCount = randomCount.Next(1, 12);
+
+Console.WriteLine($"\nOuter Random No. : {currentCount}\n");
+do
+{
+Console.WriteLine($"\nInner Random No. : {currentCount}\n");
+    currentCount = randomCount.Next(1, 20);
+Console.WriteLine($"\nNew Inner Random No. : {currentCount}\n");
+
+    if (currentCount >= 8) continue;
+
+    Console.WriteLine($"While Block with Continue Statement : {currentCount}");
+    Console.WriteLine();
+} while (currentCount != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
---
+++ DoWhile/Drill/battleGame/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\nBattle Game!\n");
+
+int heroHealth = 10 ;
+int monsterHealth = 10 ;
+int heroLost = 0;
+int monsterLost = 0;
+
+Console.WriteLine($"\nHero was damaged and lost {heroLost} health and now has {heroHealth} health.");
+
+Random battleDice = new Random();
+int heroTurn = 0;
+int monsterTurn = 0;
+Console.WriteLine(battleTurn);
+
+do
+    {
+
+        heroTurn = battleDice.Next(1,11);
+        monsterHealth = monsterHealth - heroTurn;
+        Console.WriteLine($"Hero Turn : {heroTurn} ::: Monster Health {monsterHealth}")
+    }
+    while (monsterHealth <= 7);
+
+    monsterLost = monsterLost 
+
+Console.WriteLine($"Monster was damaged and lost {monsterLost} health and now has {monsterHealth} health.\n");

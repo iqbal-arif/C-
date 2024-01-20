ddf19e96961d9c8144aa5cc6a576118d549ef574
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-20 00:37:43 -0600

    Initial Commit

---
+++ BooleanExpressions/Concept/booleanExpression/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Boolean Expressions!\n");
+//Equality Operator
+Console.WriteLine("Equality Operator Excersise\n");
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+//Turns False because of space character
+Console.WriteLine("a" == "a ");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine("\n",value1.Trim().ToLower() == value2.Trim().ToLower());
+
+
+//InEquality Operator
+Console.WriteLine("Equality Operator Excersise\n");
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue2 = "a";
+Console.WriteLine(myValue2 != "a");
+
+
+//Comparison Operator
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+// Method that Retursn Boolean Sting Data Type
+// Contains, StartWith, EndsWith
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow\n"));
+
+
+//Logical Negation
+
+// These two lines of code will create the same output
+Console.WriteLine("\n");
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+
+//Conditional Operator
+
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
---
+++ BooleanExpressions/Drill/conditionalOperator/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conditional Operator!\n");
+
+Random randomNumber = new Random();
+int percentDice = randomNumber.Next(1,50);
+
+string diceResult = percentDice > 25 ? "heads" : "tails";
+Console.WriteLine($"The Dice Result is {percentDice} :  {diceResult}\n");
+
+
+// int daysUntilExpiration = random.Next(12);
+
+string permission = "Admin|Manager";
+int adminLevel = 55;
+int managerLevel = 20;
+// int userLevel;
+
+Random random = new Random();
+int userLevel = random.Next(100);
+
+
+// Business Rules
+
+/* 1. If the user is an Admin with a level greater than 55, output the message:
+Output
+
+Welcome, Super Admin user.
+
+*/
+
+Console.WriteLine($"User Admin : {userLevel}");
+Console.WriteLine(permission.Contains("Admin") && userLevel > adminLevel ?  "Welcome, Super Admin user" : "Welcome, Admin user.");
+
+
+Console.WriteLine($"\nUser Manager : {userLevel}");
+Console.WriteLine(permission.Contains("Manager") && userLevel > managerLevel ?  "Contact an Admin for access." : "You do not have sufficient privileges.");
+
+
+Console.WriteLine($"\nUser : {userLevel}");
+if ((permission != "Admin") || (permission != "Manager")) Console.WriteLine("You do not have sufficient privileges.");

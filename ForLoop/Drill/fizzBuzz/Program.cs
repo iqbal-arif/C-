be555da0e1bdad930a7dbe8e495e3c67a143cfc2
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-20 10:14:41 -0600

    Added FizzBuzz challenge

--- ForLoop/Drill/fizzBuzz/Program.cs
+++ ForLoop/Drill/fizzBuzz/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Complete a challenge activity using for and if statements!\n");
+
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - Fizz Buzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else 
+        Console.WriteLine($"{i}");
+}

c599877c5c96e5de0b1c4f8c4221bbb1fc16088a
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-01 23:19:04 -0600

    Validate Integer Initail Commit

---
+++ DoWhile/Drill/validateInteger/validateInteger/validateInteger/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***Validate Integer!***\n");
+
+
+
+int? readInput;
+
+bool isValidNumber = true;
+
+
+do
+{
+
+    Console.WriteLine("Enter an Integer value between 5 and 10 ?\n");
+
+        readInput = Convert.ToInt32(Console.ReadLine());
+
+        //What if someone place an integer??????
+
+      //  Convert.ToInt32(readInput);
+
+    if (((readInput) >= 5) && ((readInput) <= 10))
+    {
+        Console.WriteLine("Your Input has been accepted!");
+        isValidNumber = false;
+    }
+    else if ((readInput) < 5 || (readInput) < 10)
+    {
+        Console.WriteLine("Nope, Your Integer Value is not between 5 and 10. Try it a try");
+    }
+
+    //break;
+    
+
+} while (isValidNumber);
